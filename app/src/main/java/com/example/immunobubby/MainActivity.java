22c701c7d214ab787cc6e9c879d4bf9ed40c804a
Alfonso Maria Alloro <alloroalfonso@gmail.com>
2025-09-02 19:55:02 +0200

    frequenza sintomo 2

--- app/src/main/java/com/example/immunobubby/MainActivity.java
+++ app/src/main/java/com/example/immunobubby/MainActivity.java
@@ -351,11 +351,11 @@ public class MainActivity extends BaseActivity {
     }
     
     //sharedpreferences
-    private List<Reazione> loadReazioniFromPrefs() {
+   /* private List<Reazione> loadReazioniFromPrefs() {
         SharedPreferences prefs = getContext().getClass("app_prefs", Context.MODE_PRIVATE);
         String json = prefs.getString("reazioni_list", "[]");
         Gson gson = new Gson();
         return gson.fromJson(json, new TypeToken<List<Reazione>>(){}.getType());
-    }
+    }*/
 
 }
